4b0329e9e3b71da445efdb7055e55839840a8eba
bozeeetopic <btopic00@fesb.hr>
2021-11-08 18:29:30 +0100

    Odigran prvi potez

--- TrecaIgra/Program.cs
+++ TrecaIgra/Program.cs
@@ -11,7 +11,7 @@ namespace TrecaIgra
 
             Console.WriteLine("   |   |   ");
             Console.WriteLine("   |   |   ");
-            Console.WriteLine("   |   |   ");
+            Console.WriteLine("   |   | x ");
         }
     }
 }

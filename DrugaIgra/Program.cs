0e59ebcc46a3a6da0ab0b7d4a8c3cee2facbb9c9
bozeeetopic <btopic00@fesb.hr>
2021-11-09 00:46:44 +0100

    Ponovni pokušaj, reklo je da sam 2 commita ispri 1 iza

--- DrugaIgra/Program.cs
+++ DrugaIgra/Program.cs
@@ -6,8 +6,8 @@ namespace DrugaIgra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("[ime_igrača] ima x pobjeda i y poraza!");
-            Console.WriteLine("[ime_igrača] ima z pobjeda i n poraza!");
+            Console.WriteLine("Bože ima x pobjeda i y poraza!");
+            Console.WriteLine("Marin ima z pobjeda i n poraza!");
 
             Console.WriteLine("   |   | x ");
             Console.WriteLine("   |   | x ");
